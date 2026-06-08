ce1ba8da13b1e8e222b2b35869317c3a9ac3a9f6
SadikMR <sadikmahmudraihan@gmail.com>
2026-06-08 10:37:11 +0600

    Update Go module path to GitHub repository

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	_ "travelshpere-sadik/routers"
+	_ "github.com/SadikMR/travelshpere-sadik/routers"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 package routers
 
 import (
-	"travelshpere-sadik/controllers"
+	"github.com/SadikMR/travelshpere-sadik/controllers"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
