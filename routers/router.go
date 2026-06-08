b69e6a593ec4e95f66eb11f0a56a9ca182a8cabf
SadikMR <sadikmahmudraihan@gmail.com>
2026-06-08 10:32:05 +0600

    Initialized the Project

---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"travelshpere-sadik/controllers"
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
